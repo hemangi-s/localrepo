001c145ed2e8fc5bd3153e56aaebab9a5965d955
Hemangi <hemangisabalpara11@gmail.com>
2024-01-10 22:28:10 +0530

    js foundation

---
+++ main.js
@@ -0,0 +1,34 @@
+console.log("hello");
+
+var a = 1;
+
+console.log(a);
+
+const b = 2;
+
+console.log(b);
+
+
+// data types
+
+let fName = "hey";
+let age = 25;
+let isDay = true;
+
+
+console.log("" +fName+ " there your age is " +age+ "");
+
+if(isDay == true){
+    console.log("day");
+}
+else{
+    console.log("night");
+}
+
+
+
+for(let i=0; i<10; i++){
+    i;
+}
+
+console.log(i);
